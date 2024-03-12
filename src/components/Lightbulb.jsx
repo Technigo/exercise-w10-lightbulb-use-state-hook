6a85123b31700bf63accb16dc983f76cdcb1e3e7
Maria-source-cell <manuelabirnat@gmail.com>
2024-03-12 13:50:08 +0100

    solution W6-exercise
    Co-authored-by: Johanna Billingskog Nyberg <JohannaBN@users.noreply.github.com>
    Co-authored-by: Frida Svensson <fridamarisvensson@gmail.com>
    Co-authored-by: Erica Mechler <erica.mechler1@gmail.com>
    Co-authored-by: Arnau Vidal <vidalhuix@users.noreply.github.com>

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react'
+
 export const Lightbulb = () => {
   // Initialize the state for on/off
+  const [toggle, setToggle] = useState('lightbulb off')
+  
 
   const toggleLight = () => {
-    // Toggle the on/off state
+    // Toggle the on/off state 
     console.log("Clicked");
+    if (toggle === 'lightbulb off') {
+    setToggle('lightbulb on')
+  } else {
+    setToggle('lightbulb off')
+  }
   };
+
   return (
     <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
+      <div className={toggle} onClick={toggleLight}>
         <div className="bulb"></div>
       </div>
     </div>
   );
 };
+
